c2cf0960f1404fd55fd9e7b5bd8a2cede20c6987
DLInchausti <54633218+DLInchausti@users.noreply.github.com>
2021-05-10 22:08:13 -0300

    Add files via upload

---
+++ Farmacia/Farmacia/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farmacia
+{
+    class Cliente : Persona
+    {
+        
+
+
+        public Cliente(string apellido, string nombre, int dni, Domicilio domicilio, ObraSocial obrasocial) : base(apellido, nombre, dni, domicilio, obrasocial)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.dni = dni;
+            this.domicilio = domicilio;
+            this.obrasocial = obrasocial;
+        }
+
+        public Cliente()
+        {
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Farmacia/Farmacia/Domicilio.cs
@@ -0,0 +1,28 @@
+﻿namespace Farmacia
+{
+    public class Domicilio
+    {
+        public string calle { get; set; }
+        public int numeroCalle { get; set; }
+        public string localidad { get; set; }
+        public string provincia { get; set; }
+
+        public Domicilio(string calle, int numeroCalle, string localidad, string provincia)
+        {
+            this.calle = calle;
+            this.numeroCalle = numeroCalle;
+            this.localidad = localidad;
+            this.provincia = provincia;
+        }
+
+        public Domicilio()
+        {
+           
+        }
+
+
+
+
+
+    }
+}
